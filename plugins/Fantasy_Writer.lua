4455fed24fdea05cba003cb571468ae96415fef3
p00ria <amir1300sobatdar@gmail.com>
2016-06-01 14:41:02 +0430

    Update Fantasy_Writer.lua

--- plugins/Fantasy_Writer.lua
+++ plugins/Fantasy_Writer.lua
@@ -106,7 +106,7 @@ local function run(msg, matches)
 		a=a+1
 		result_text = result_text..a.."- "..result[a].."\n\n"
 	end
-	return result_text.."______________________________\nSignal Bot"
+	return result_text.."______________________________\n@SignalTM"
 end
 
 return {
