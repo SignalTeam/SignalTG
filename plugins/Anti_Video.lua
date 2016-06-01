e2ddcf9f024728cfab08436b828e010fe02f9490
p00ria <amir1300sobatdar@gmail.com>
2016-06-01 14:56:44 +0430

    Create Anti_Video.lua

---
+++ plugins/Anti_Video.lua
@@ -0,0 +1,29 @@
+kicktable = {}
+
+
+local function run(msg, matches)
+    if is_momod(msg) then
+        return msg
+    end
+    local data = load_data(_config.moderation.data)
+    if data[tostring(msg.to.id)] then
+        if data[tostring(msg.to.id)]['settings'] then
+            if data[tostring(msg.to.id)]['settings']['lock_video'] then
+                lock_video = data[tostring(msg.to.id)]['settings']['lock_video']
+            end
+        end
+    end
+    local chat = get_receiver(msg)
+    local user = "user#id"..msg.from.id
+    if lock_video == "yes" then
+        send_large_msg(get_receiver(msg), "User @" .. msg.from.username .. " Send video is not allowed here!")
+        chat_del_user(chat, user, ok_cb, true)
+    end
+end
+ 
+return {
+  patterns = {
+  "%[(video)%]"
+ },
+  run = run
+}
