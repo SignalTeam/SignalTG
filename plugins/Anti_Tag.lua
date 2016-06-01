66bc3f311d88506b6ff19146b2489b5d242439aa
p00ria <amir1300sobatdar@gmail.com>
2016-06-01 14:46:45 +0430

    Create Anti_Tag.lua

---
+++ plugins/Anti_Tag.lua
@@ -0,0 +1,26 @@
+ocal function run(msg, matches)
+    if is_owner(msg) then
+        return
+    end
+    local data = load_data(_config.moderation.data)
+    if data[tostring(msg.to.id)] then
+        if data[tostring(msg.to.id)]['settings'] then
+            if data[tostring(msg.to.id)]['settings']['antitag'] then
+                lock_tag = data[tostring(msg.to.id)]['settings']['antitag']
+            end
+        end
+    end
+    local chat = get_receiver(msg)
+    local user = "user#id"..msg.from.id
+    if lock_tag == "yes" then
+        send_large_msg(chat, 'شما تگ فرستادید و این در گروه ممنوع است\nشما اخراج می شوید')
+        chat_del_user(chat, user, ok_cb, true)
+    end
+end
+ 
+return {
+  patterns = {
+    "#(.*)",
+  },
+  run = run
+}
