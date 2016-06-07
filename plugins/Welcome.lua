6054fa2d51fd8b9ebb89b30b445fa75ac90e637e
p00ria <amir1300sobatdar@gmail.com>
2016-06-07 10:06:42 +0430

    Update Welcome.lua

--- plugins/Welcome.lua
+++ plugins/Welcome.lua
@@ -1,104 +1,56 @@
-local add_user_cfg = load_from_file('data/add_user_cfg.lua')
-
-local function template_add_user(base, to_username, from_username, chat_name, chat_id)
-   base = base or ''
-   to_username = '@' .. (to_username or '')
-   from_username = '@' .. (from_username or '')
-   chat_name = string.gsub(chat_name, '_', ' ') or ''
-   chat_id = "chat#id" .. (chat_id or '')
-   if to_username == "@" then
-      to_username = ''
-   end
-   if from_username == "@" then
-      from_username = ''
-   end
-   base = string.gsub(base, "{to_username}", to_username)
-   base = string.gsub(base, "{from_username}", from_username)
-   base = string.gsub(base, "{chat_name}", chat_name)
-   base = string.gsub(base, "{chat_id}", chat_id)
-   return base
+do
+local function run(msg, matches, callback, extra)
+local data = load_data(_config.moderation.data)
+local rules = data[tostring(msg.to.id)]['rules']
+local about = data[tostring(msg.to.id)]['description']
+local hash = 'group:'..msg.to.id
+local group_welcome = redis:hget(hash,'welcome')
+if matches[1] == 'delwlc' and not matches[2] and is_owner(msg) then 
+    
+   redis:hdel(hash,'welcome')
+        return 'متن خوش آمد گویی پاک شد 🗑'
 end
 
-function chat_new_user_link(msg)
-   local pattern = add_user_cfg.initial_chat_msg
-   local to_username = msg.from.username
-   local from_username = 'link (@' .. (msg.action.link_issuer.username or '') .. ')'
-   local chat_name = msg.to.print_name
-   local chat_id = msg.to.id
-   pattern = template_add_user(pattern, to_username, from_username, chat_name, chat_id)
-   if pattern ~= '' then
-      local receiver = get_receiver(msg)
-      send_msg(receiver, pattern, ok_cb, false)
-   end
+local url , res = http.request('http://api.gpmod.ir/time/')
+if res ~= 200 then return "No connection" end
+local jdat = json:decode(url)
+
+if matches[1] == 'setwlc' and is_owner(msg) then
+redis:hset(hash,'welcome',matches[2])
+        return 'متن خوش آمد گویی گروه تنظیم شد به : \n'..matches[2]
 end
 
-function chat_new_user(msg)
-   local pattern = add_user_cfg.initial_chat_msg
-   local to_username = msg.action.user.username
-   local from_username = msg.from.username
-   local chat_name = msg.to.print_name
-   local chat_id = msg.to.id
-   pattern = template_add_user(pattern, to_username, from_username, chat_name, chat_id)
-   if pattern ~= '' then
-      local receiver = get_receiver(msg)
-      send_msg(receiver, pattern, ok_cb, false)
-   end
-end
+if matches[1] == 'chat_add_user' or 'chat_add_user_link' or 'channel_invite' and msg.service then
+group_welcome = string.gsub(group_welcome, '{gpname}', msg.to.title)
+group_welcome = string.gsub(group_welcome, '{firstname}', ""..(msg.action.user.first_name or '').."")
+ group_welcome = string.gsub(group_welcome, '{lastname}', ""..(msg.action.user.last_name or '').."")
+  group_welcome = string.gsub(group_welcome, '{username}', "@"..(msg.action.user.username or '').."")
+  group_welcome = string.gsub(group_welcome, '{fatime}', ""..(jdat.FAtime).."")
+  group_welcome = string.gsub(group_welcome, '{fadate}', ""..(jdat.FAdate).."")
+  group_welcome = string.gsub(group_welcome, '{rules}', ""..(rules or '').."")
+  group_welcome = string.gsub(group_welcome, '{about}', ""..(about or '').."")
 
-local function description_rules(msg, nama)
-   local data = load_data(_config.moderation.data)
-   if data[tostring(msg.to.id)] then
-      local about = ""
-      local rules = ""
-      if data[tostring(msg.to.id)]["description"] then
-         about = data[tostring(msg.to.id)]["description"]
-         about = "\nAbout :\n"..about.."\n"
-      end
-      if data[tostring(msg.to.id)]["rules"] then
-         rules = data[tostring(msg.to.id)]["rules"]
-         rules = "\nRules :\n"..rules.."\n"
-      end
-      local sambutan = "Hi "..nama.." Welcome to "..string.gsub(msg.to.print_name, "_", " ")..
-      local text = sambutan..about..rules.."\n"
-      local receiver = get_receiver(msg)
-      send_large_msg(receiver, text, ok_cb, false)
-   end
-end
 
-local function run(msg, matches)
-   if not msg.service then
-      return "Are you trying to troll me?"
-   end
-   --vardump(msg)
-   if matches[1] == "chat_add_user" then
-      if not msg.action.user.username then
-          nama = string.gsub(msg.action.user.print_name, "_", " ")
-      else 
-          nama = "@"..msg.action.user.username
-      end
-      chat_new_user(msg)
-      description_rules(msg, nama)
-   elseif matches[1] == "chat_add_user_link" then
-      if not msg.from.username then
-          nama = string.gsub(msg.from.print_name, "_", " ")
-      else
-          nama = "@"..msg.from.username
-      end
-      chat_new_user_link(msg)
-      description_rules(msg, nama)
-   elseif matches[1] == "chat_del_user" then
-       local bye_name = "@"..msg.action.user.username
-       return 'Bye '..bye_name..
-   end
-end
 
+
+group_welcome = string.gsub(group_welcome, '{نام گروه}', msg.to.title)
+group_welcome = string.gsub(group_welcome, '{نام اول}', ""..(msg.action.user.first_name or '').."")
+ group_welcome = string.gsub(group_welcome, '{نام آخر}', ""..(msg.action.user.last_name or '').."")
+  group_welcome = string.gsub(group_welcome, '{نام کاربری}', "@"..(msg.action.user.username or '').."")
+  group_welcome = string.gsub(group_welcome, '{ساعت فارسی}', ""..(jdat.FAtime).."")
+  group_welcome = string.gsub(group_welcome, '{تاریخ فارسی}', ""..(jdat.FAdate).."")
+
+ end
+return group_welcome
+end
 return {
-   description = "Welcoming Message",
-   usage = "send message to new member",
-   patterns = {
-      "^!!tgservice (chat_add_user)$",
-      "^!!tgservice (chat_add_user_link)$",
-      "^!!tgservice (chat_del_user)$",
-   },
-   run = run
+  patterns = {
+  "^[!#/](setwlc) +(.*)$",
+  "^[!#/](delwlc)$",
+  "^!!tgservice (chat_add_user)$",
+  "^!!tgservice (channel_invite)$",
+  "^!!tgservice (chat_add_user_link)$",
+  },
+  run = run
 }
+end
