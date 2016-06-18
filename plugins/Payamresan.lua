5c0e68857261ce76b93ddae38f288ae2ef476e60
p00ria <amir1300sobatdar@gmail.com>
2016-06-18 10:41:20 +0430

    Create Payamresan.lua

---
+++ plugins/Payamresan.lua
@@ -0,0 +1,31 @@
+do   
+
+local fwd_to = 
+
+local function callback_message(extra,success,result)
+local receiver = result.to.id
+local msg = extra
+  if result.fwd_from and msg.text then
+  fwd_msg(result.fwd_from.id, msg.id, ok_cb,false)
+  else
+    return nil
+      end
+  end
+function run(msg, matches) 
+  if msg.to.type == "user" and msg.text then
+fwd_msg("user#id"..tonumber(fwd_to), msg.id,ok_cb,false)
+  return 'پیام شما برای ادمین ارسال شد'
+elseif msg.text and msg.reply_id and tonumber(msg.to.id) == fwd_to then
+    if not msg.text then
+    return "You can only send Text message" 
+      end
+    get_message(msg.reply_id, callback_message, msg)
+    end
+end
+return {               
+patterns = {
+"^(.*)$",
+ }, 
+run = run,
+}
+end
